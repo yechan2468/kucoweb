6165d065426197b813c2c64c4a02acd2b90aec4e
Yechan Lee <yechan24680@gmail.com>
2023-03-19 18:36:09 +0900

    set up spring boot and react.js dev environment

---
+++ accountant/src/main/java/kuco/accountant/controller/HomeController.java
@@ -0,0 +1,13 @@
+package kuco.accountant.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/api/hello")
+    public String Hello(Model model) {
+        return "Hello, World!";
+    }
+}
---
+++ accountant/src/test/java/kuco/accountant/AccountantApplicationTests.java
@@ -0,0 +1,13 @@
+package kuco.accountant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
